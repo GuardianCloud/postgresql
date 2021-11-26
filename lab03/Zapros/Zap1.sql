26eb9dbf927234d4c871cc594ed9382883f961bd
Student <alekseypanin06@gmail.com>
2021-11-26 20:06:23 +0300

    adasdasdasd

---
+++ lab03/Zapros/Zap1.sql
@@ -0,0 +1,2 @@
+SELECT country FROM games 
+WHERE Standing = 'Europe champinoship' AND Year_of BETWEEN '1900' and '2006';
---
+++ lab03/Zapros/Zap2.sql
@@ -0,0 +1,2 @@
+SELECT Team_name FROM team_members
+WHERE games.Standing = 'Olympic Games' AND games.Kind_of_sport = 'Volleyball';
